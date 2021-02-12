25dfc5380746a609636fee647c03384c6c410fe7
Max Cohen <max.zagouri@pm.me>
2021-02-12 17:11:55 +0100

    Add torch dataset

---
+++ src/dataset.py
@@ -0,0 +1,21 @@
+import torch
+from torch.utils.data import Dataset
+
+
+class ARMADataset(Dataset):
+    def __init__(self, y, u, T):
+        n_chunk = len(y) // T
+
+        _y = torch.Tensor(y[: n_chunk * T])
+
+        if len(_y.shape) == 1:
+            _y = _y.unsqueeze(-1)
+
+        self._y = _y.chunk(n_chunk)
+        self._u = u.chunk(n_chunk)
+
+    def __getitem__(self, idx):
+        return (self._u[idx], self._y[idx])
+
+    def __len__(self):
+        return len(self._y)
