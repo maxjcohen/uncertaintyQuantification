f99841046675439f61962d5acb210207a05ef826
Max Cohen <max.zagouri@pm.me>
2021-02-23 14:39:59 +0100

    Update main script

--- main.py
+++ main.py
@@ -1,27 +1,90 @@
-import math
-
-import torch
-import torch.nn as nn
 import numpy as np
+import matplotlib.pyplot as plt
+from statsmodels.tsa.arima_process import arma_generate_sample
+import torch
+from torch.utils.data import DataLoader
+import pytorch_lightning as pl
+from tqdm import tqdm
 
 from ucn.models import SMCN
+from src.trainer import SMCNTrainer
+from src.dataset import ARMADataset
 
-# Shapes
-T = 5
-d_emb = 4
-d_out = 2
 
-N = 3
+def compare_stuff(model):
+    print(
+        "Sigma_x:\t",
+        model._sigma_x.diag().detach().square().numpy().squeeze().round(2),
+        f"\t({sigma_x})",
+    )
+    print(
+        "Sigma_y:\t",
+        model._sigma_y.diag().detach().square().numpy().squeeze().round(2),
+        f"\t({sigma_y})",
+    )
+    print(
+        "a:\t\t",
+        model._g._linear.weight.detach().numpy().squeeze().round(2),
+        f"\t({b})",
+    )
+    print(
+        "b:\t\t", model._f._linear.weight.detach().numpy().squeeze().round(2), "\t(1)"
+    )
 
-bs = 2
 
-# Inputs and observations
-u_tild = torch.randn(T, bs, d_emb)
-u_tild  # (T, bs, d_emb)
+# Define model
+b = 0.75
 
-y = torch.randn(T, bs, d_out)
-y  # (T, bs, d_out)
+sigma_x = 0.6
+sigma_y = 0.2
+
+n_obs = 10000
+
+# Load model
+arparams = np.array([b])
+maparams = np.array([0])
+
+arparams = np.r_[1, -arparams]
+maparams = np.r_[1, maparams]
+x_arma = arma_generate_sample(arparams, maparams, n_obs, scale=sigma_x)
+
+y_arma = (
+    x_arma
+    + np.random.multivariate_normal(
+        (0,), np.diag([sigma_y]), size=len(x_arma)
+    ).squeeze()
+)
+
+# Plot ARMA
+plt.figure(figsize=(25, 5))
+plt.plot(y_arma[:250], label="observation")
+plt.plot(x_arma[:250], lw=3, label="state")
+plt.legend()
+plt.title("ARMA model")
+
+
+# Load model
+d_emb = 1
+d_out = 1
+N = 50
 
 model = SMCN(d_emb, d_out, n_particles=N)
 
-model(u_tild, noise=True)
+# Load dataset
+T = 25
+batch_size = 8
+epochs = 30
+
+dataset = ARMADataset(y_arma, torch.zeros(len(y_arma), d_emb), T)
+dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=4, shuffle=True)
+
+# Print initial state
+compare_stuff(model)
+
+# Train
+train_model = SMCNTrainer(model)
+trainer = pl.Trainer(max_epochs=epochs, gpus=0)
+trainer.fit(train_model, dataloader)
+
+# Print final state
+compare_stuff(model)
