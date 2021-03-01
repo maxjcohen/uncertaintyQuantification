1c409bcd0c38105cd509ddebe3336a65f60f7e91
Max Cohen <max.zagouri@pm.me>
2021-03-01 09:53:56 +0100

    Re parametrize _sigma_x as log(sigma^2)
    Access sigma^2 is possible via model.sigma_x, no matter the future
    re parametrisations

--- ucn/models.py
+++ ucn/models.py
@@ -30,14 +30,22 @@ class SMCN(nn.Module):
         self._g = FFN(self._input_size, self._input_size)
 
         self._sigma_x = nn.Parameter(
-            torch.cholesky(torch.diag(torch.rand(self._input_size))), requires_grad=True
+            torch.log(torch.diag(torch.rand(self._input_size))), requires_grad=True
         )
         self._sigma_y = nn.Parameter(
-            torch.cholesky(torch.diag(torch.rand(self._output_size))),
+            torch.log(torch.diag(torch.rand(self._output_size))),
             requires_grad=True,
         )
 
         self.softmax = nn.Softmax(dim=1)
+
+    @property
+    def sigma_x(self):
+        return torch.exp(self._sigma_x)
+
+    @property
+    def sigma_y(self):
+        return torch.exp(self._sigma_y)
 
     def forward(self, u, y=None, noise=False):
         # N should be 1 if there is no noise
@@ -53,7 +61,7 @@ class SMCN(nn.Module):
 
         # Initial hidden state
         x = torch.zeros(bs, self.N, self._input_size, device=u.device)
-        self._eta = MultivariateNormal(torch.zeros(x.shape), scale_tril=self._sigma_x)
+        self._eta = MultivariateNormal(torch.zeros(x.shape), covariance_matrix=self.sigma_x)
 
         # Generat initial particules
         x = self._g(x)
@@ -64,7 +72,7 @@ class SMCN(nn.Module):
         y_hat = self._f(x)
         predictions.append(y_hat)
         if fisher:
-            self._normal_y = MultivariateNormal(y[0], scale_tril=self._sigma_y)
+            self._normal_y = MultivariateNormal(y[0], covariance_matrix=self.sigma_y)
             self.w = self._normal_y.log_prob(y_hat.transpose(0, 1)).T.detach()
             self.w = self.softmax(self.w)
 
@@ -85,7 +93,7 @@ class SMCN(nn.Module):
             y_hat = self._f(x)
             predictions.append(y_hat)
             if fisher:
-                self._normal_y = MultivariateNormal(y[k], scale_tril=self._sigma_y)
+                self._normal_y = MultivariateNormal(y[k], covariance_matrix=self.sigma_y)
                 self.w = self._normal_y.log_prob(y_hat.transpose(0, 1)).T.detach()
                 self.w = self.softmax(self.w)
         return torch.stack(predictions)
@@ -124,11 +132,11 @@ class SMCN(nn.Module):
         particules = self.smooth_pms(self.particules, self.I).detach()
 
         # Compute likelihood
-        normal_y = MultivariateNormal(y.unsqueeze(-2), scale_tril=self._sigma_y)
+        normal_y = MultivariateNormal(y.unsqueeze(-2), covariance_matrix=self.sigma_y)
         loss_y = -normal_y.log_prob(self._f(particules)) * self.w.detach()
         loss_y = loss_y.sum(-1)
 
-        normal_x = MultivariateNormal(particules[1:], scale_tril=self._sigma_x)
+        normal_x = MultivariateNormal(particules[1:], covariance_matrix=self.sigma_x)
         loss_x = -normal_x.log_prob(self._g(particules[:-1])) * self.w.detach()
         loss_x = loss_x.sum(-1)
 
